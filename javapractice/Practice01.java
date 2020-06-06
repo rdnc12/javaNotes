e4f8b2696bdf4503e689c8bd2cc40fd2ed5046f6
Erdinc <Erdinc@gklfmg.com>
2020-06-06 22:41:40 +0200

    first week review

---
+++ javapractice/Practice01.java
@@ -0,0 +1,25 @@
+package javapractice;
+
+public class Practice01 {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// declaring variable
+		// data type+variable name = value;
+		boolean b;
+		char c = 0;
+		byte by = 0;
+		short s = 0;
+		int i = 0;
+		long l = 0;
+		float f = 0f;
+		double d = 0;
+
+		// To assign a value
+		b = true;
+
+		// we have to initialize the variables to print the values.
+		System.out.println(c);
+	}
+
+}
