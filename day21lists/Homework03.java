cb90933054563199f82bbbe9ddf9818d0f73614d
Erdinc <Erdinc@gklfmg.com>
2020-06-29 16:02:11 +0200

    list exercises

---
+++ day21lists/Homework03.java
@@ -0,0 +1,21 @@
+package day21lists;
+
+public class Homework03 {
+
+	public static void main(String[] args) {
+		 // Find the sum of all elements in the multi dimensional array { {1,2,3},
+        // {4,5,6} }
+        int arr[][] = { { 1, 2, 3 }, { 4, 5, 6 } };
+        
+        int sum = 0;
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j<arr[i].length; j++) {
+                sum+=arr[i][j];
+            }
+        }
+        System.out.println(sum);
+
+
+	}
+
+}
