b3636d240238c6be0e78ba9f78b135496ae03c92
Erdinc <Erdinc@gklfmg.com>
2020-07-21 17:52:47 +0200

    interface and abstract exp

---
+++ day35interface/Explanation.java
@@ -0,0 +1,20 @@
+package day35interface;
+
+public class Explanation {
+/*
+ * Differences between Abstract Class and Interface
+ 1) Method Differences:
+    ==> Abstract classes can have both abstract and concrete methods but
+        interfaces normally can have just abstract methods.
+        After Java 8, we can use "default" and "static" keywords to create
+        concrete methods in an interface
+ 2) Variable Differences:
+    ==> Abstract classes can have final or non-final, static or non-static variables. But interfaces can have just final(must be initialized) and just static varibles.
+    ==> Abstract classes can have public, protected, default, private variables but interfaces can have just public variables.
+ 3) Inheritance Differences:
+    ==> We use "extends" keyword to inherit from a class(abstract or not) to a class(abstract or not).
+    We use "implements" keyword to inherit from a  class(abstract or not) to interface.
+    We use "extends" keyword to inherit from an interface to an interface.
+    An interface cannot accept a class as parent
+    ==> We cannot create multiple inheritance between classes but multiple inheritance is supported by Java when we use interfaces*/
+}
